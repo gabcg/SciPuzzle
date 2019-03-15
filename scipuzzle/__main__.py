6cac4e67997a7be10b1e5e9f2d0ed7194be95f9a
Aina Rill <ainarill11@gmail.com>
2019-03-15 17:30:19 +0100

    kkkk

--- scipuzzle/__main__.py
+++ scipuzzle/__main__.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from Bio.PDB import *
+
+parser = PDBParser()
+structure = parser.get_structure('X', '../example_inputs/pair_his3_sc_XA.pdb')
+for model in structure:
+    for chain in model:
+        for residue in chain:
+            for atom in residue:
+                print(atom)
+
+ppb = CaPPBuilder()
+for pp in ppb.build_peptides(structure):
+    print(pp.get_sequence())
