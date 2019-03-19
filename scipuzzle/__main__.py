e5c21fe0457749810f4591fcf115cd3c98b274bc
luisas <luisa.santus95@gmail.com>
2019-03-19 14:24:25 +0100

    Arguments finished - first attempts in getting structures and filtering out the interacting pairs

--- scipuzzle/__main__.py
+++ scipuzzle/__main__.py
@@ -1,61 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
+import arguments
+import utils
 
-from Bio.PDB import *
-import os
-from os import path
-import itertools
-from Bio import pairwise2
+# STEP 1: parse the arguments
+options = arguments.read_args()
+input_files = arguments.get_input_files(options.input)
+stoichiometry = None
+if options.stoichiometry is not None:
+    stoichiometry = arguments.parse_stoichiometry(options.stoichiometry)
+if options.verbose:
+    sys.err("Input correctly parsed.\nFiles used as input:")
+    for file in input_files:
+        sys.err(file)
 
-
-
-
-
-verbose = True
-
-input_pdb = "../example_inputs/pair_his3_sc_XA.pdb"
-input_folder = "/Users/luisasantus/Documents/GitHub/SciPuzzle/example_inputs/"
-
-
-
-def chain_to_fasta(chain):
-    ppb=PPBuilder()
-    for pp in ppb.build_peptides(chain):
-        return pp.get_sequence()
-
-def get_input_files():
-    for i in os.listdir(input_folder):
-        #TODO: remove the second part before submitting!
-        if i.endswith('.pdb') and  not "chain" in i:
-            pdb_files.append(os.path.join(input_folder, i))
-    return pdb_files
-
-
-def collect_chains(input_file):
-    chains = []
-    parser = PDBParser(QUIET=True)
-    structure = parser.get_structure('X', input_file)
-    for model in structure:
-        for chain in model:
-            chains.append(chain)
-    return chains[]
-
-# A-B B-C C-D
-
-# A, B, B , C, C, D
-
+# STEP2: Get possible structures for Macrocomplex construction and skip others
 pairs = []
-for file in get_input_files():
+for file in input_files:
+    pairs.append(utils.get_chains(file))
+pairs = utils.filter_pairs(pairs)
+print(pairs)
 
 
+# STEP3: Check for stoichiometry requirements
+chains = []
+if not utils.stoichiometry_is_possible(stoichiometry, chains):
+    # TODO: change with trow exception!
+    exit()
 
-
-
-
-for chain_one, chain_two  in itertools.combinations(chains, 2):
-    sequence_identity_threshold = 0.95
-    aligner = Align.PairwiseAligner()
-    alignments = pairwise2.align.globalxx(chain_to_fasta(chain_one), chain_to_fasta(chain_two))
-    pairwise_sequence_identity = alignments[0][2] /len(alignments[0][0])
-    if pairwise_sequence_identity >= sequence_identity_threshold:
-        print(pairwise_sequence_identity)
+# STEP4: Begin Macrocomplex reconstruction!
