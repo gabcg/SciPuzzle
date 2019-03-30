55f5ffa18a62fd814fdf392dfbe6bf1f5eea8c90
luisas <luisa.santus95@gmail.com>
2019-03-30 10:30:22 +0100

    lala

--- scipuzzle/__main__.py
+++ scipuzzle/__main__.py
@@ -31,8 +31,8 @@ structures = {}
 chain_index = 1
 for file in input_files:
     paired_chains = []
-    structure = utils.get_structure(file)
-    for chain in utils.get_chain_from_structure(structure):
+    structure = utils.get_structure(file, remove_het = True)
+    for chain in utils.get_chain_from_structure(structure, remove_het = True):
         chain_id = str(chain_index)+"_"+str(chain.id)
         chains[chain_id] = chain
         chain.id = chain_id
@@ -56,27 +56,65 @@ print(line)
 #    raise exceptions.IncorrectStoichiometry(stoichiometry=stoichiometry)
 
 
+one = utils.get_structure("/Users/luisasantus/Documents/GitHub/SciPuzzle/example_2/pair_his3_sc_XA.pdb")
+two = utils.get_structure("/Users/luisasantus/Documents/GitHub/SciPuzzle/example_2/pair_his3_sc_XB.pdb")
 
-# def test(pair_one, pair_two):
-#     (superimposed, rmsd) = utils.superimpose(pair_one,pair_two)
-#     #pair_one.add()
-#     utils.write_structure_into_mmcif(pair_one, "test.cif")
-#
-# print("Here i am ")
+def test(pair_one, pair_two):
+
+    print("lala")
+    chains = {}
+    chain_index = 1
+    for chain in utils.get_chain_from_structure(pair_one):
+        chain_id = str(chain_index)+"_"+str(chain.id)
+        chain.id = chain_id
+        chains[chain_id] = chain
+    chain_index += 1
+    print("lala2")
+    for chain in utils.get_chain_from_structure(pair_two):
+        chain_id = str(chain_index)+"_"+str(chain.id)
+        chain.id = chain_id
+        chains[chain_id] = chain
+    similar_chains = utils.get_similar_chains(chains)
+    print("chains "+ str(chains))
+    print(similar_chains)
+
+    for c in utils.get_chain_from_structure(pair_one):
+        if c.id in similar_chains:
+            similar = similar_chains[c.id][0]
+            print(c.id)
+            print(similar)
+    for a in utils.get_chain_from_structure(pair_two):
+        if similar != a.id:
+            other = a.id
+    print("pther "+ other)
+
+    matrix = utils.superimpose_chains_test(utils.get_chain(pair_one,c.id),utils.get_chain(pair_two, similar))
+    matrix.apply(utils.get_chain(pair_two,other))
+    #(superimposed, rmsd) = utils.superimpose(pair_one,pair_two)
+    utils.write_structure_into_mmcif(pair_one, "test1.cif")
+    utils.write_structure_into_mmcif(pair_two, "test2.cif")
+#test(one,two)
 
 print("going further ")
 # STEP4: Begin Macrocomplex reconstruction!
-def construct_complex(current_complex, chains,
+def construct_complex(current_complex_real, chains,
                       similar_chains, stoichiometry, pairs,
                       structures, used_pairs):
     # bruteforce ending!
+    current_complex = copy.deepcopy(current_complex_real)
     if current_complex is not None:
-        if len(utils.get_chain_from_structure(current_complex)) == 3:
+        for chain in utils.get_chain_from_structure(current_complex):
+            print("Chain id: " + str(chain.id) + " --> " + str(chain))
+
+    #utils.print_chain_in_structure(current_complex)
+    if current_complex is not None:
+        if utils.complex_fits_stoich(current_complex,stoichiometry):
+        #if len(utils.get_chain_from_structure(current_complex)) == 3:
             print(line)
             print("Recursion Finished!")
             for chain in utils.get_chain_from_structure(current_complex):
                 print(chain)
-            utils.write_structure_into_mmcif(current_complex, "lala.cif")
+            utils.write_structure_into_mmcif(current_complex, "output.cif")
             return [current_complex]
     # current_complex is a list of chains
     # for the first round its going to be a random pair of chains.
@@ -99,34 +137,39 @@ def construct_complex(current_complex, chains,
         ps = utils.get_possible_structures(chain_in_current_complex,
                                            similar_chains, structures)
         print("Possible structures : "+ str(ps))
-        for ps_id in ps:
-            structure_id = ps[ps_id]
+        for similar_chain_id in ps:
+            structure_id = ps[similar_chain_id]
             structure_to_superimpose = structures[structure_id]
             print("Superimposing")
             print("Structure to superimpose: "+ str(structure_to_superimpose))
             # Superimpose current complex with one of the possible structures.
-            (superimposed, rmsd) = utils.superimpose(current_complex,structure_to_superimpose)
-            # print("ps id " + str(ps_id))
-            # print("chain in cu co "+ str(chain_in_current_complex))
-            for chain in utils.get_chain_from_structure(superimposed):
-                if chain.id != ps_id:
-                    print("Trying with: "+ str(chain.id))
-                    # check if are clashing
-                    if structure_id not in used_pairs:
-                        #if not utils.are_clashing(current_complex, chains[chain.id]):
+            matrix = utils.superimpose_chains_test(utils.get_chain(current_complex,chain_in_current_complex)
+                                                  ,utils.get_chain(structure_to_superimpose, similar_chain_id))
+            other = [tuple_id for tuple_id in structure_id if tuple_id != similar_chain_id][0]
+            matrix.apply(utils.get_chain(structure_to_superimpose,other))
+            current_complex[0].add(utils.get_chain(structure_to_superimpose,other))
+            used_pairs.append(structure_id)
+            construct_complex(current_complex, chains,
+                              similar_chains, stoichiometry, pairs,
+                              structures, used_pairs)
 
-                            current_complex[0].add(utils.get_chain(superimposed, chain.id))
-                            used_pairs.append(structure_id)
-                            print("no clash found!")
-                            print("Adding: "+ str(chain.id))
-                            print("Used pairs "+str(used_pairs))
-                            construct_complex(current_complex, chains,
-                                              similar_chains, stoichiometry, pairs,
-                                              structures, used_pairs)
-                        #else:
-                            #print("Clash was found :(")
-                    else:
-                        print("already in used pairs")
+            # for chain in utils.get_chain_from_structure(superimposed):
+            #     if chain.id != similar_chain_id:
+            #         print("Trying with: "+ str(chain.id))
+            #         # check if are clashing
+            #         if structure_id not in used_pairs:
+            #             #if not utils.are_clashing(current_complex, chains[chain.id]):
+            #                 #current_complex[0].add(utils.get_chain(superimposed, chain.id))
+            #                 #matrix.apply(utils.get_chain(pair_two,other))
+            #                 used_pairs.append(structure_id)
+            #                 print("no clash found!")
+            #                 print("Adding: "+ str(chain.id))
+            #                 print("Used pairs "+str(used_pairs))
+            #
+            #             #else:
+            #                 #print("Clash was found :(")
+            #         else:
+            #             print("already in used pairs")
 
 
 test_complex = construct_complex(None, chains, similar_chains,
--- scipuzzle/utils.py
+++ scipuzzle/utils.py
@@ -5,6 +5,7 @@ import Bio.PDB as pdb
 from Bio import pairwise2
 import itertools
 import exceptions
+import copy
 import __main__ as main
 
 
@@ -18,14 +19,23 @@ def chain_to_fasta(chain):
         return pp.get_sequence()
 
 
-def get_structure(input_file):
+def get_structure(input_file, remove_het=False):
     """
     Extracts the structure from a pdb file.
     Returns the structure.
     """
     parser = pdb.PDBParser(QUIET=True)
     structure = parser.get_structure('X', input_file)
-    return structure
+    if not remove_het:
+        return structure
+    else:
+        for model in structure:
+            for chain in model:
+                heteroatoms = list(filter(lambda x: x.id[0] != " ",
+                                          chain.get_residues()))
+                for heteroatom in heteroatoms:
+                    chain.detach_child(heteroatom.id)
+        return structure
 
 
 def get_chains(input_file):
@@ -46,6 +56,8 @@ def get_chains(input_file):
     return chains
 
 
+
+
 def get_similar_chains(chains, sequence_identity_threshold=0.95):
     """
     Compute which chains are similar to which ones given a dictionary having
@@ -64,7 +76,7 @@ def get_similar_chains(chains, sequence_identity_threshold=0.95):
                                               chain_to_fasta(chains[chain_2]))
         pairwise_sequence_identity = alignments[0][2]/len(alignments[0][0])
         if pairwise_sequence_identity >= sequence_identity_threshold:
-            (superimposed, rmsd) = superimpose(chains[chain_1], chains[chain_2])
+            (superimposed, rmsd) = superimpose_chains(chains[chain_1], chains[chain_2])
             print(rmsd)
             if rmsd < 0.05:
                 if chain_1 not in similar_chains:
@@ -124,11 +136,18 @@ def are_clashing(chain_one, chain_two, max_clashes=50, contact_distance=1.0):
     return False
 
 
-def get_chain_from_structure(structure):
+def get_chain_from_structure(structure, remove_het = False):
     chains = []
     for model in structure:
         for chain in model:
-            chains.append(chain)
+            if not remove_het:
+                chains.append(chain)
+            else:
+                heteroatoms = list(filter(lambda x: x.id[0] != " ",
+                                          chain.get_residues()))
+                for heteroatom in heteroatoms:
+                    chain.detach_child(heteroatom.id)
+                chains.append(chain)
     return chains
 
 def get_chain(structure, chain_id):
@@ -161,12 +180,59 @@ def remove_chain(structure, chain_id):
         model.detach_child(chain_id)
     return structure
 
-def superimpose(structure_one, structure_two):
+
+def superimpose_chains_test(chain_one_real, chain_two_real):
     """
     Superimposes two structures and returns the superimposed structure and the rmsd
     of the superimposition.
     """
+    chain_one = copy.deepcopy(chain_one_real)
+    chain_two = copy.deepcopy(chain_two_real)
+    super_imposer = pdb.Superimposer()
+    atoms_one = sorted(list(chain_one.get_atoms()))
+    atoms_two = sorted(list(chain_two.get_atoms()))
+    # Fix lengths so that they are the same
+    min_len = min(len(atoms_one), len(atoms_two))
+    atoms_one = atoms_one[:min_len]
+    atoms_two = atoms_two[:min_len]
+    super_imposer.set_atoms(atoms_one, atoms_two)
+    return super_imposer
 
+
+def print_chain_in_structure(structure):
+    if structure is not None:
+        for chain in get_chain_from_structure(structure):
+            print("Chain id: " + str(chain.id) + " --> " + str(chain))
+
+
+def superimpose_chains(chain_one_real, chain_two_real):
+    """
+    Superimposes two structures and returns the superimposed structure and the rmsd
+    of the superimposition.
+    """
+    chain_one = copy.deepcopy(chain_one_real)
+    chain_two = copy.deepcopy(chain_two_real)
+    super_imposer = pdb.Superimposer()
+    atoms_one = sorted(list(chain_one.get_atoms()))
+    atoms_two = sorted(list(chain_two.get_atoms()))
+    # Fix lengths so that they are the same
+    min_len = min(len(atoms_one), len(atoms_two))
+    atoms_one = atoms_one[:min_len]
+    atoms_two = atoms_two[:min_len]
+    super_imposer.set_atoms(atoms_one, atoms_two)
+    super_imposer.apply(list(chain_two.get_atoms()))
+    return (chain_two, super_imposer.rms)
+
+
+
+
+def superimpose(structure_one_real, structure_two_real):
+    """
+    Superimposes two structures and returns the superimposed structure and the rmsd
+    of the superimposition.
+    """
+    structure_one = copy.deepcopy(structure_one_real)
+    structure_two = copy.deepcopy(structure_two_real)
     super_imposer = pdb.Superimposer()
     atoms_one = list(structure_one.get_atoms())
     atoms_two = list(structure_two.get_atoms())
@@ -175,7 +241,7 @@ def superimpose(structure_one, structure_two):
     atoms_one = atoms_one[:min_len]
     atoms_two = atoms_two[:min_len]
     super_imposer.set_atoms(atoms_one, atoms_two)
-    super_imposer.apply(structure_two.get_atoms())
+    super_imposer.apply(list(structure_two[0].get_atoms()))
     return (structure_two, super_imposer.rms)
 
 def get_all_similar_pairs(pair, similar_chains, structures):
@@ -258,7 +324,7 @@ def write_structure_into_pdb_old(chains, name):
 
 
 def complex_fits_stoich(complex, stoichiometry):
-    if len(complex) == sum(stoichiometry.values()):
+    if len(get_chain_from_structure(complex)) == sum(stoichiometry.values()):
         return True
     else:
         return False
